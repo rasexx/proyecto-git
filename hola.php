de51ba1aedee8e36f8f960e4dfcbb70bff8b76e3
rasexx <contacto.itsras@gmail.com>
2024-03-01 02:49:48 -0500

    Se añade el archivo .txt con las instrucciones del profesor para el taller y se cierra la actividad publicandola en github

--- hola.php
+++ hola.php
@@ -1,3 +1,4 @@
 <?php
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
-@print "Hola, {$nombre}\n";
+     // El nombre por defecto es Mundo
+     $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+     @print "Hola, {$nombre}\n";
